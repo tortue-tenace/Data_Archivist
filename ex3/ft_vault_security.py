60d9a8146539c69d25e974401203a27d7828d3a3
thattal <thattal@f6r7s6.paris.42.school>
2026-06-24 17:23:05 +0200

    save

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -1,29 +1,26 @@
 print("=== Cyber Archives Security ===\n")
-def secure_archive(filename: str, action: str = "r", content: str ="")->tuple:
-    if action != "r" or action != "w":
+
+
+def secure_archive(
+    filename: str, action: str = "r", content: str = ""
+) -> tuple[bool, str]:
+    if action != "r" and action != "w":
         return (False, "Invalid usage of function")
-    if action == 'r':
+    if action == "r":
         try:
-            print(f"Using 'secure_archive to read from {filename} file:")
+            print(f"Using 'secure_archive' to read from '{filename}' file:")
             with open(filename, "r") as file:
-                contenu = file.read()
-                return (True, contenu)
-        except FileNotFoundError:
-            return (False, f"[Errno 2] No such file or directory: '{filename}'")
-        except PermissionError:
-            return (False, f"[Errno 13] Permission denied: '{filename}'")
-    elif action == 'w':
-        try:
-            print(f"Using 'secure_archive' to write previous content to a new file")
-            with open(filename) as file:
-                contenu = file.read()
-                with open("file", "w") as new_file:
-                    if content == "":
-                        new_file.write(contenu)
-                    else:
-                        new_file.write(content)
-            return (True, "Content successfully written to file")
-        except Exception as e:
-            return (False, f"{e}") 
-    
-print(secure_archive("test.txt", "a", "wessd"))
+                return (True, file.read())
+        except OSError as e:
+            return (False, f"{e}")
+    try:
+        print(f"Using 'secure_archive' to write content to '{filename}':")
+        with open(filename, "w") as file:
+            file.write(content)
+        return (True, "Content successfully written to file")
+    except OSError as e:
+        return (False, f"{e}")
+
+
+print(secure_archive("test.txt", "w", "wessd"))
+print(secure_archive("test.txt", "r"))
